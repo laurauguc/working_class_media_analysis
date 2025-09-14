bbc17bd6ee979aa6e90b604adf2fb660ca50a3dd
laurauguc <l.uguccioni1@gmail.com>
2025-09-13 19:35:30 -0700

    significant progress

--- analysis/utils.py
+++ analysis/utils.py
@@ -51,22 +51,23 @@ def compare_histograms(items_list1, items_list2, title, filename, label1='List 1
     plt.show()
 
 # Set of U.S. states
-us_states = {
-    'Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut',
-    'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa',
-    'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan',
-    'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
-    'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio',
-    'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota',
-    'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia',
-    'Wisconsin', 'Wyoming'
-}
-
-world_countries = [c.name for c in pycountry.countries if c.name not in ["Jersey", "Georgia"]] + ["UK", "Britain", "England", "South Korea", "North Korea", "Iran", "Syria", "Venezuela", "Russia", "Taiwan", "Saint Martin", "Iran", "Lao", "Moldova", "Tanzania", "Vatican", "Venezuela", "Korea"]
 
 
 # Function to split states and countries
 def split_locations(locations):
+    us_states = {
+        'Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut',
+        'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa',
+        'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan',
+        'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
+        'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio',
+        'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota',
+        'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia',
+        'Wisconsin', 'Wyoming'
+    }
+
+    world_countries = [c.name for c in pycountry.countries if c.name not in ["Jersey", "Georgia"]] + ["UK", "Britain", "England", "South Korea", "North Korea", "Iran", "Syria", "Venezuela", "Russia", "Taiwan", "Saint Martin", "Iran", "Lao", "Moldova", "Tanzania", "Vatican", "Venezuela", "Korea"]
+
     states = []
     countries = []
     other = []
@@ -92,6 +93,47 @@ def split_locations(locations):
 from itertools import chain
 
 # Flatten and filter location lists
-def c(series, exclude="NA"):
-    """Flatten lists in a pandas Series and filter out unwanted values."""
-    return [item for item in chain.from_iterable(series.dropna()) if item != exclude]
+#def flatten_and_filter(series, exclude="NA"):
+#    """Flatten lists in a pandas Series and filter out unwanted values."""
+#    return [item for item in chain.from_iterable(series.dropna()) if item != exclude]
+
+def flatten(series):
+    """Flatten lists in a pandas Series"""
+    return [item for item in chain.from_iterable(series.dropna())]
+
+
+import re
+
+def extract_region_mentions(text, regions=None):
+    """
+    Extract mentions of specified U.S. regions from the given text.
+
+    Parameters
+    ----------
+    text : str
+        The input text to search.
+    regions : list of str, optional
+        A list of region names to search for. If None, defaults to:
+        ['Northeast', 'South', 'Midwest', 'Southwest', 'West']
+
+    Returns
+    -------
+    list of str
+        Mentions of regions found in the text, preserving order of appearance.
+        Special rule: mentions of 'Mid-Atlantic' are mapped to 'Northeast'.
+    """
+
+    mentions = []
+    if regions is None:
+        regions = ["Northeast", "South", "Midwest", "Southwest", "West"]
+
+        # Special rule: Mid-Atlantic counts as Northeast
+        if re.search(r"\bMid-?Atlantic\b", text): # flags=re.IGNORECASE
+            mentions.append("Northeast")
+
+    for region in regions:
+        pattern = r'\b' + re.escape(region) + r'\b'
+        if re.search(pattern, text): # flags=re.IGNORECASE
+            mentions.append(region)
+
+    return mentions
